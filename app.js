139c354f7e5fad0e6cb674c53c98dc9088d2952e
nduytung <nduytung.1611@gmail.com>
2022-05-16 20:25:57 +0700

    testing heroku deploy

--- app.js
+++ app.js
@@ -23,10 +23,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/auth", authRouter);
 app.use("/sensor", sensorRouter);
-app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
